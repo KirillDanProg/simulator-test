b78ce6c301a3de420ef1ace61666b11aca4dffa5
KirillDanProg <kirill2898v@gmail.com>
2022-11-24 22:04:25 +0300

    slice added for questions

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {simulatorSlice} from "../features/test-simulator/simulator-slice";
+import {questionsSlice} from "../features/test-simulator/questions-slice";
 
 export const store = configureStore({
     reducer: {
-        test: simulatorSlice
-    }
+        test: simulatorSlice,
+        questions: questionsSlice
+    },
 })
 
 export type RootState = ReturnType<typeof store.getState>;
