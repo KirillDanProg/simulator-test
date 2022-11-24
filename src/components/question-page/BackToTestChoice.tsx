bdac70a7263cee4513b3f4341de7aae453b268e5
KirillDanProg <kirill2898v@gmail.com>
2022-11-24 22:02:07 +0300

    redirect to start page added

--- src/components/question-page/BackToTestChoice.tsx
+++ src/components/question-page/BackToTestChoice.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import {Flex} from "../../common-components/Flex";
 import {ArrowBack} from "../../assets/icons/ArrowBack";
+import {useNavigate} from "react-router-dom";
 
 export const BackToTestChoice = () => {
+
+    const navigate = useNavigate()
+
+    const backToStartPage = () => {
+        navigate("/")
+    }
+
     return (
-        <Flex align={"center"} gap={"12px"}>
-            <ArrowBack size={"18px"}
-                       color={"black"}
-            />
-            <span>Назад к выбору теста</span>
-        </Flex>
+        <div onClick={backToStartPage}>
+            <Flex position={"top-left"} align={"center"} gap={"12px"} padding={"30px 0"}>
+                <ArrowBack size={"18px"}
+                           color={"black"}
+                />
+                <span style={{cursor: "pointer"}}>Назад к выбору теста</span>
+            </Flex>
+        </div>
+
     );
 };
 
