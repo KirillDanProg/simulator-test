2b9de1109dfd5297b120986552183ac25a448d32
KirillDanProg <kirill2898v@gmail.com>
2022-11-24 10:12:53 +0300

    project structure created

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css'
+import {StartPage} from "./components/start-page/StartPage";
+import styled from "styled-components";
+import {Provider} from "react-redux";
+import {store} from "./app/store";
+import { Route, Routes} from "react-router-dom";
+import QuestionContainer from "./components/question-page/QuestionContainer";
+import Header from "./components/Header";
+
+const StyledApp = styled.div`
+  display: grid;
+  place-items: center;
+  min-height: 100vh;
+
+`
+
+function App() {
+
+    return (
+        <Provider store={store}>
+
+            <StyledApp>
+                <Header/>
+                <Routes>
+                    <Route index element={<StartPage/>}/>
+                    <Route path={"test"} element={<QuestionContainer/>}/>
+                </Routes>
+            </StyledApp>
+
+        </Provider>
+    )
+}
+
+export default App
