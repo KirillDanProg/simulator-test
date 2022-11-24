c82af7840c45eb314c4cd76670b0c0f60c7c100e
KirillDanProg <kirill2898v@gmail.com>
2022-11-24 22:05:37 +0300

    test type choice added with dispatch setTestType

--- src/components/start-page/StartPage.tsx
+++ src/components/start-page/StartPage.tsx
@@ -3,21 +3,37 @@ import front from "../../assets/front.png"
 import back from "../../assets/back.png"
 import {Link} from "react-router-dom";
 import {StyledStartPage} from "./StyledStartPage";
+import {useAppDispatch} from "../../app/hooks";
+import {setTestType} from "../../features/test-simulator/simulator-slice";
+import {Flex} from "../../common-components/Flex";
 
 
 export const StartPage = () => {
+
+    const dispatch = useAppDispatch()
+
+    const onClickHandler = (type: "front" | "back") => {
+        dispatch(setTestType({value: type}))
+    }
+
     return (
         <StyledStartPage>
-            <h1>Выбери направление</h1>
-            <Link to={"test"}>
-                <img className={"img"}
-                     src={front}/>
-            </Link>
 
-            <Link to={"test"}>
-                <img className={"img"}
-                     src={back}/>
-            </Link>
+            <h1>Выбери направление</h1>
+            <Flex padding={"100px 0"}
+                gap={"100px"}
+            >
+                <Link to={"test"} onClick={() => onClickHandler("front")}>
+                    <img className={"img"}
+                         src={front}/>
+                </Link>
+
+                <Link to={"test"} onClick={() => onClickHandler("back")}>
+                    <img className={"img"}
+                         src={back}/>
+                </Link>
+            </Flex>
+
 
         </StyledStartPage>
     );
