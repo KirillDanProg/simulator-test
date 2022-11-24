aa562e3d6cba75a4b3e631d5e50b3b9fb4efedca
KirillDanProg <kirill2898v@gmail.com>
2022-11-24 22:06:31 +0300

    dot props ProgressBar.tsx updated

--- src/components/question-page/ProgressBar.tsx
+++ src/components/question-page/ProgressBar.tsx
@@ -1,39 +1,62 @@
-import React, {useState} from 'react';
+import React, {FC, useState} from 'react';
 import styled from "styled-components";
+import {StatusType} from "../../features/test-simulator/questions-slice";
+import {useAppSelector} from "../../app/hooks";
 
-const qs = [
-    {id: 1,}, {id: 2,}, {id: 3,}, {id: 4,}
-]
 
-const StyledDot = styled.div`
+type StepDotPropsType = {
+    setCur: (cur: number) => void
+    // todo fix type
+    id: any
+    status: StatusType
+}
+
+const StyledDot = styled.div<StepDotPropsType>`
   width: 10px;
   height: 10px;
   border-radius: 50%;
-  background-color: red;
+  background-color: lightgray;
+  
+  ${props => props.status === "right" && `
+    background-color: green;
+  `}
+  ${props => props.status === "wrong" && `
+    background-color: red;
+  `}
+  ${props => props.status === "idle" && `
+  
+    background-color: white;
+    border: 1px solid blue;
+  `}
 `
-const StepDot = (props: {setCur: (cur: number) => void, id: number}) => {
-    return (
-        <StyledDot onClick={() => props.setCur(props.id)}>
 
-        </StyledDot>
+
+const StepDot: FC<StepDotPropsType> = (props) => {
+    return (
+        <StyledDot onClick={() => props.setCur(props.id)}
+                   {...props}
+        />
     )
 }
 
 const StyledStepper = styled.div`
   display: flex;
   gap: 10px;
-  margin: 20px;
+  align-items: center;
 `
 export const Stepper = () => {
-
     const [cur, setCur] = useState(1)
+    const questions = useAppSelector(state => state.questions.questions)
 
     return (
         <StyledStepper>
 
-            {`${cur} из ${qs.length}` }
+            {`${cur} из ${questions.length}`}
 
-            {qs.map(dot => <StepDot id={dot.id}  setCur={setCur}/>)}
+            {questions.map(dot => <StepDot key={dot.id}
+                                    id={dot.id}
+                                    setCur={setCur}
+                                    status={dot.status}/>)}
 
         </StyledStepper>
     )
