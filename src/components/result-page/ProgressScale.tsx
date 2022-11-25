da570952ddc5d59051bf27a0bd5c572fb3f6b91a
KirillDanProg <kirill2898v@gmail.com>
2022-11-25 17:55:21 +0300

    circular progress bar created

---
+++ src/components/result-page/ProgressScale.tsx
@@ -0,0 +1,48 @@
+import React, {FC} from 'react';
+import styled from "styled-components";
+
+const StyledProgressScale = styled.div<ProgressScalePropsType>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .circ-progress {
+    width: 170px;
+    height: 170px;
+    background: conic-gradient(#3BB98A ${props => props.value * 3.6}deg, lightgray 0deg);
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .circ-progress::before {
+    content: "";
+    position: absolute;
+    width: 140px;
+    height: 140px;
+    border-radius: 50%;
+    background-color: #fff;
+  }
+  .progress-value {
+    position: relative;
+    font-size: 40px;
+  }
+`
+
+type ProgressScalePropsType = {
+    value: number
+}
+
+export const ProgressScale: FC<ProgressScalePropsType> = (props) => {
+    return (
+        <StyledProgressScale {...props}>
+            <div className={"circ-progress"}>
+                <div className={"progress-value"}>
+                    {`${props.value}%`}
+                </div>
+            </div>
+
+        </StyledProgressScale>
+    );
+};
+
