01b26689e10bd96180ac1a11ffe3771fca15985e
KirillDanProg <kirill2898v@gmail.com>
2022-11-25 09:21:02 +0300

    RTK changed to legacy redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,30 @@
-import {configureStore} from "@reduxjs/toolkit";
-import {simulatorSlice} from "../features/test-simulator/simulator-slice";
-import {questionsSlice} from "../features/test-simulator/questions-slice";
+import {
+    AnyAction,
+    applyMiddleware,
+    combineReducers,
+    compose,
+    legacy_createStore,
+    ThunkAction
+} from "@reduxjs/toolkit";
+import {questionsReducer} from "../features/test-simulator/questions-reducer";
+import thunk from "redux-thunk";
+import {appReducer} from "../features/test-simulator/app-reducer";
 
-export const store = configureStore({
-    reducer: {
-        test: simulatorSlice,
-        questions: questionsSlice
-    },
+const rootReducer = combineReducers({
+    questions: questionsReducer,
+    app: appReducer
 })
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = legacy_createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk),
+));
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export type AppThunk<ReturnType = any> = ThunkAction<ReturnType,
+    RootState,
+    unknown,
+    AnyAction>
