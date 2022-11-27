7d3be27446c73f24c0c08e942887ad57c7c3f6d3
KirillDanProg <kirill2898v@gmail.com>
2022-11-27 23:04:59 +0300

    questions taken from store/last question condition updated

--- src/components/question-page/QuestionContainer.tsx
+++ src/components/question-page/QuestionContainer.tsx
@@ -1,37 +1,46 @@
 import React, {useEffect, useState} from 'react';
 import {BackToTestChoice} from "./BackToTestChoice";
 import {AnswerVariant} from "./AnswerVariant";
-import {Container} from "../../common-components/Container";
-import {Button} from "../../common-components/Button";
 import {Stepper} from "./ProgressBar";
 import {StyledQuestionContainer} from "./StyledTestSimulator";
-import {Flex} from "../../common-components/Flex";
 import {useAppDispatch, useAppSelector} from "../../app/hooks";
 import {Questions} from "../../features/test-simulator/Questions";
-import {changeQuestionStatus, fetchQuestions, QuestionType} from "../../features/test-simulator/questions-reducer";
-import Title from "../../common-components/Title";
+import {
+    changeQuestionStatus,
+    fetchQuestions,
+    saveChosenAnswer
+} from "../../features/test-simulator/questions-reducer";
 import {useNavigate} from "react-router-dom";
+import {Button, Container, Flex, Title} from "../../common-components";
+import {Nullable} from "../../app/types";
+import {Preloader} from "../../common-components/Preloader";
 
 export const QuestionContainer = () => {
     const dispatch = useAppDispatch()
     const navigate = useNavigate()
 
-    const [currentCheckbox, setCurrentCheckbox] = useState<number | null>(null)
+    const [ansId, setAnsId] = useState<Nullable<number>>(null)
+    const [currentCheckbox, setCurrentCheckbox] = useState<Nullable<number>>(null)
     const [questionId, setQuestionId] = useState(0)
-    const [ansId, setAnsId] = useState<number | null>(null)
+    const testType = useAppSelector(state => state.app.testType)
 
-    const currentQuestion: QuestionType = Questions[questionId]
-    // todo change condition
-    if (currentQuestion === undefined) {
-        navigate("/result")
-    }
-    const answersKeys = Object.keys(currentQuestion.possibleAnswers)
+    const questions = useAppSelector(state => state.questions.questions)
+    const currentQuestion = questions[questionId]
+
+    const answersKeys = currentQuestion && Object.keys(currentQuestion.possibleAnswers)
 
     const goToNextQuestion = (skip: string) => {
-
-        setQuestionId((state) => state + 1)
+        // todo change condition
         setCurrentCheckbox(null)
 
+        setQuestionId((state) => {
+            if (state + 1 > questions.length - 1) {
+                navigate("/result")
+                return state
+            } else {
+                return state + 1
+            }
+        })
         if (skip) {
             dispatch(changeQuestionStatus(currentQuestion.id, "idle"))
             return
@@ -40,60 +49,59 @@ export const QuestionContainer = () => {
             dispatch(changeQuestionStatus(currentQuestion.id, "right"))
         } else if (`${ansId}` !== currentQuestion.rightAnswer) {
             dispatch(changeQuestionStatus(currentQuestion.id, "wrong"))
+            dispatch(saveChosenAnswer(currentQuestion.id, ansId))
         }
 
     }
 
-    const testType = useAppSelector(state => state.app.testType)
 
     useEffect(() => {
-        dispatch(fetchQuestions(Questions))
+        setTimeout(() => {
+            dispatch(fetchQuestions(Questions))
+        }, 1000)
+
     }, [])
 
     return (
         <Container width={"756px"}>
-            <StyledQuestionContainer>
+            {
+                !currentQuestion
+                    ? <Preloader/>
+                    : <StyledQuestionContainer>
 
-                <BackToTestChoice/>
+                        <BackToTestChoice/>
+                        <Title type={testType}/>
+                        <Stepper questionId={currentQuestion.id}/>
 
-                    <Title type={testType}/>
+                        <div>
+                            {currentQuestion.body}
+                        </div>
 
-                    <Stepper questionId={currentQuestion.id}/>
+                        <div>
+                            {
+                                answersKeys.map((el: string) => {
+                                    return (
+                                        <AnswerVariant key={el}
+                                                       id={+el}
+                                                       setCurrentCheckbox={setCurrentCheckbox}
+                                                       currentCheckbox={currentCheckbox}
+                                                       ans={currentQuestion.possibleAnswers}
+                                                       selectAnswer={setAnsId}
+                                        />
+                                    )
+                                })}
+                        </div>
 
-
-                <div>
-                    {currentQuestion.body}
-                </div>
-
-                <div>
-                    {answersKeys.map((el: string) => {
-                        return (
-                            <AnswerVariant key={el}
-                                           id={+el}
-                                           setCurrentCheckbox={setCurrentCheckbox}
-                                           currentCheckbox={currentCheckbox}
-                                           ans={currentQuestion.possibleAnswers}
-                                           selectAnswer={setAnsId}
-                            />
-
-                        )
-                    })}
-                </div>
-
-
-                <Flex direction={"row"} justify={"space-between"} gap={"5px"}>
-                    <Button callback={() => goToNextQuestion("skip")}
-                            padding={"6px 18px"}
-                            variant={"outlined"}>Пропустить вопрос</Button>
-                    <Button callback={() => goToNextQuestion("")}
-                            padding={"6px 18px"}
-                            variant={"contained"}>Ответить</Button>
-                </Flex>
-
-            </StyledQuestionContainer>
+                        <Flex direction={"row"} justify={"space-between"} gap={"5px"}>
+                            <Button callback={() => goToNextQuestion("skip")}
+                                    padding={"6px 18px"}
+                                    variant={"outlined"}>Пропустить вопрос</Button>
+                            <Button callback={() => goToNextQuestion("")}
+                                    padding={"6px 18px"}
+                                    variant={"contained"}>Ответить</Button>
+                        </Flex>
+                    </StyledQuestionContainer>
+            }
         </Container>
-
     );
 };
-
-
