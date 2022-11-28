dbc1f8922d8a0fc59c8198e1de6fd591cbf95c04
KirillDanProg <kirill2898v@gmail.com>
2022-11-28 18:59:38 +0300

    wrapped with Wrapper.tsx

--- src/components/question-page/BackToTestChoice.tsx
+++ src/components/question-page/BackToTestChoice.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import {Flex} from "../../common-components/Flex";
+import {Flex} from "../../common-components";
 import {ArrowBack} from "../../assets/icons/ArrowBack";
 import {useNavigate} from "react-router-dom";
+import {Wrapper} from "../../common-components/Wrapper";
 
 export const BackToTestChoice = () => {
 
     const navigate = useNavigate()
 
-    const backToStartPage = () => {
-        navigate("/")
-    }
+    const backToStartPage = (): void => navigate("/")
 
     return (
-        <div onClick={backToStartPage}>
-            <Flex direction={"row"} position={"top-left"} align={"center"} gap={"12px"} padding={"30px 0"}>
+        <Wrapper callback={backToStartPage}>
+            <Flex direction={"row"}
+                  position={"top-left"}
+                  align={"center"}
+                  gap={"12px"}
+                  padding={"30px 0"}
+            >
                 <ArrowBack size={"18px"}
                            color={"black"}
                 />
                 <span style={{cursor: "pointer"}}>Назад к выбору теста</span>
             </Flex>
-        </div>
-
+        </Wrapper>
     );
 };
-
